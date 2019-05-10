6675f81112388be2ff78020764f8c2de27763e4c
cuixianming <cuixianming@gotokeep.com>
2019-05-10 19:07:03 +0800

    init commit

---
+++ app/src/main/java/com/gotokeep/su/widget/checkcalandarview/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gotokeep.su.widget.checkcalandarview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
